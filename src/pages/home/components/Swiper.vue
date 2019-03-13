297a210c0726b81811e443e1152ad72dd2cdc621
Ye <931450539@qq.com.com>
2019-03-13 16:47:21 +0800

    add icons

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -21,9 +21,15 @@ export default {
       swiperList: [{
         id: '0001',
         imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/0a8f7c789d0e62966e2485d33bf7ebd3.jpg_750x200_b455610d.jpg'
-      },{
-         id: '0002',
+      }, {
+        id: '0002',
         imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/2f5f3ea4698c9b7898db7562d89b91ed.jpg_750x200_bd3b4ce9.jpg'
+      }, {
+        id: '0003',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/892f011c3b7bd6815b460c659acc5651.jpg_750x200_e2242cfb.jpg'
+      }, {
+        id: '0004',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/85f0bd47e09b753456b96bad681745ed.jpg_750x200_ae385290.jpg'
       }]
     }
   }
@@ -33,6 +39,11 @@ export default {
 <style lang="stylus" scoped>
     .wrapper >>> .swiper-pagination-bullet-active
         background: #fff
+    .wrapper
+      overflow: hidden
+      width: 100%
+      height: 0
+      padding-bottom: 26%
     .swiper-img
       width: 100%
 </style>
